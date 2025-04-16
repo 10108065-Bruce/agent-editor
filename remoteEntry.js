a0c275e5a305d3e08c33424cf37aa565f30e5c42
Bruce Tseng <bruce.tseng@quantatw.com>
2025-04-16 10:51:49 +0800

    Updates

--- remoteEntry.js
+++ remoteEntry.js
@@ -1,7 +1,7 @@
 window.drawingApp = window.drawingApp || {};
 
 import { importShared } from './assets/__federation_fn_import-CpyXGjFi.js';
-import FlowEditor, { j as jsxRuntimeExports } from './assets/__federation_expose_FlowEditor-B9TrPrXS.js';
+import FlowEditor, { j as jsxRuntimeExports } from './assets/__federation_expose_FlowEditor-M1XkzEou.js';
 import { r as requireReact, g as getDefaultExportFromCjs } from './assets/index-DvUXrXSS.js';
 import { r as requireReactDom } from './assets/index-CHD0Wkh7.js';
 
