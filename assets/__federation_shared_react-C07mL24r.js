9af4a6d867b481aeee42813eb27366869edfa42a
Bruce Tseng <bruce.tseng@quantatw.com>
2025-04-17 08:43:53 +0800

    Updates

---
+++ assets/__federation_shared_react-C07mL24r.js
@@ -0,0 +1,8 @@
+window.drawingApp = window.drawingApp || {};
+
+import { r as requireReact, g as getDefaultExportFromCjs } from './index-DvUXrXSS.js';
+
+var reactExports = requireReact();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactExports);
+
+export { index as default };
---
+++ env-config.js
@@ -0,0 +1,7 @@
+// Development version generated at 2025-04-17T00:43:43.008Z
+window.ENV = {
+  VITE_APP_VERSION: "0.1.4",
+  VITE_APP_BUILD_TIME: "2025-04-17T00:43:43.010Z",
+  VITE_APP_BUILD_ID: "dev-1744850623010",
+  MODE: "development"
+};
