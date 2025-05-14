02931e87e4df9493f6287e50320dd55123b6c905
Bruce Tseng <bruce.tseng@quantatw.com>
2025-05-14 13:32:54 +0800

    [modify] 刪除用不到的程式

--- public/env-config.js
+++ public/env-config.js
@@ -1,7 +1,7 @@
-// Development version generated at 2025-05-14T01:27:31.924Z
+// Development version generated at 2025-05-14T02:26:10.002Z
 window.ENV = {
-  VITE_APP_VERSION: "0.1.84",
-  VITE_APP_BUILD_TIME: "2025-05-14T01:27:31.924Z",
-  VITE_APP_BUILD_ID: "dev-1747186051924",
+  VITE_APP_VERSION: "0.1.85",
+  VITE_APP_BUILD_TIME: "2025-05-14T02:26:10.003Z",
+  VITE_APP_BUILD_ID: "dev-1747189570003",
   MODE: "development"
 };
