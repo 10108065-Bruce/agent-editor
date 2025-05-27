c4b371998f8079ee9148f14df43d689b2d60734a
Bruce Tseng <bruce.tseng@quantatw.com>
2025-05-27 13:37:02 +0800

    Updates

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -5,7 +5,7 @@ const currentImports = {};
       let moduleMap = {
 "./FlowEditor":()=>{
       dynamicLoadingCss(["styles.css"], false, './FlowEditor');
-      return __federation_import('/agent-editor/assets/__federation_expose_FlowEditor-DZM-nt0Q.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      return __federation_import('/agent-editor/assets/__federation_expose_FlowEditor-CCUz__bm.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
-// Development version generated at 2025-05-27T05:36:30.464Z
+// Development version generated at 2025-05-27T05:36:51.587Z
 window.ENV = {
-  VITE_APP_VERSION: "0.1.47.0",
-  VITE_APP_BUILD_TIME: "2025-05-27T05:36:30.464Z",
-  VITE_APP_BUILD_ID: "dev-1748324190464",
+  VITE_APP_VERSION: "0.1.48.0",
+  VITE_APP_BUILD_TIME: "2025-05-27T05:36:51.588Z",
+  VITE_APP_BUILD_ID: "dev-1748324211588",
   MODE: "development"
 };
