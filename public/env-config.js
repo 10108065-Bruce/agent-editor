9bec13aaad233ec79d2c47f5533f0e760160936c
Bruce Tseng <bruce.tseng@quantatw.com>
2025-07-28 09:16:07 +0800

    [modify] Update AIM node icon design

--- public/env-config.js
+++ public/env-config.js
@@ -1,7 +1,7 @@
-// Development version generated at 2025-07-25T03:12:15.862Z
+// Development version generated at 2025-07-25T07:55:04.582Z
 window.ENV = {
   VITE_APP_VERSION: "0.1.49.1",
-  VITE_APP_BUILD_TIME: "2025-07-25T03:12:15.862Z",
-  VITE_APP_BUILD_ID: "dev-1753413135862",
+  VITE_APP_BUILD_TIME: "2025-07-25T07:55:04.582Z",
+  VITE_APP_BUILD_ID: "dev-1753430104582",
   MODE: "development"
 };
