560b82c40ed4803dde854dfd7f44597e6fba771f
Bruce Tseng <bruce.tseng@quantatw.com>
2025-10-17 14:07:26 +0800

    Updates

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
-// Development version generated at 2025-10-17T06:04:41.209Z
+// Development version generated at 2025-10-17T06:07:16.975Z
 window.ENV = {
-  VITE_APP_VERSION: "0.1.55.4",
-  VITE_APP_BUILD_TIME: "2025-10-17T06:04:41.209Z",
-  VITE_APP_BUILD_ID: "dev-1760681081209",
+  VITE_APP_VERSION: "0.1.55.5",
+  VITE_APP_BUILD_TIME: "2025-10-17T06:07:16.975Z",
+  VITE_APP_BUILD_ID: "dev-1760681236975",
   MODE: "development"
 };
